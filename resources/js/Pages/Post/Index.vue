a22d2ae35c4bb4d01f7f0aed16c4337a43e6dd4b
Clinton1998 <clintontapialagar@gmail.com>
2021-09-15 12:21:27 -0500

    Configuracion inicial de crud basico con Laravel + Inertiajs + tailwindcss

---
+++ resources/js/Pages/Post/Index.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <h1 class="text-3xl text-blue-700 text-center font-extrabold">Listado de posts</h1>
+        <table class="table table-fixed">
+            <thead>
+                <tr>
+                    <th class="w-1/12">#</th>
+                    <th class="w-8/12">Título</th>
+                    <th class="w-2/12">Categoria</th>
+                    <th class="w-1/12">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="(post, index) in posts" :key="post.id">
+                    <td>{{ index + 1 }}</td>
+                    <td><a href="#" class="underline hover:text-red-600">{{ post.title }}</a></td>
+                    <td>{{ post.category.name }}</td>
+                    <td>
+                        <button class="btn btn-warning">Editar</button>
+                        <button class="btn btn-danger">Eliminar</button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    },
+
+    created(){
+        console.log('Los datitos recibidos del servidor son: ');
+        console.log(this.posts);
+
+    }
+}
+</script>
---
+++ resources/views/app.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
+        <script src="{{ mix('/js/app.js') }}" defer></script>
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div class="container mx-auto">
+            @inertia
+        </div>
+    </body>
+</html>
